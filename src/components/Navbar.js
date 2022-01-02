6855b2eda0e451cea23a23160edfb714258165b2
Rajiv-Kumar-Singh <rv488448@gmail.com>
2022-01-02 16:49:08 +0530

    add changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar(props) {
         <>
             <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`} >
                 <div className="container-fluid">
-                    <p className="navbar-brand" >{props.title}</p>
+                    <p className="navbar-brand main-title" >{props.title}</p>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
